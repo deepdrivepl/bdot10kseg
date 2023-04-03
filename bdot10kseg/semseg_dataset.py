259b6b3b2d149d660cb27cea4cb9e161ec94772e
Aleksandra <aleksandraaa.kos@gmail.com>
2023-04-03 13:31:42 +0200

    implement binary semseg

--- bdot10kseg/semseg_dataset.py
+++ bdot10kseg/semseg_dataset.py
@@ -21,11 +21,14 @@ from base_dataset import BDOT10kDataset
 class BDOT10kSemSeg(BDOT10kDataset):
     """BDOT10k dataset"""
     def __init__(self, binary=False, **kwargs):  
-        self.binary = binary
         super(BDOT10kSemSeg, self).__init__(**kwargs)
+        self.binary = binary
+
 
     def get_label(self, df, idx):
-        mask = np.zeros((self.get_heads_count(), self.H, self.W), dtype='int')#self.size, self.size), dtype='int')
+        if self.binary:
+            return self.get_label_binary(df, idx)
+        mask = np.zeros((self.get_heads_count(), self.H, self.W), dtype='int')
         cat_names = list(self.bdot10k_cats_dict.keys())
         if df is not None:
             for code in df.X_KOD.unique():
@@ -35,21 +38,60 @@ class BDOT10kSemSeg(BDOT10kDataset):
                 m = rasterize(shapes=df[df.X_KOD==code].geometry.iloc, out_shape=(self.H, self.W))[::-1]
                 mask[out_id][m!=0] = m[m!=0]
         return mask
+
+
+    def get_label_binary(self, df, idx):
+        mask = np.zeros((1, self.H, self.W), dtype='int')
+        if df is not None:
+            for code in df.X_KOD.unique():
+                if self.classes is not None and not code.startswith(tuple(self.classes)):
+                    continue
+                m = rasterize(shapes=df[df.X_KOD==code].geometry.iloc, out_shape=(self.H, self.W))[::-1]
+                mask[0][m!=0] = m[m!=0]
+        return mask
+
     
     def plot_sample(self, img, mask, show=False):
-        cat_names = list(self.bdot10k_cats_dict.keys())
+        if self.binary:
+            self.plot_sample_binary(img, mask, show=show)
+
+        else:
+            cat_names = list(self.bdot10k_cats_dict.keys())
+            
+            n, m = 5, int(math.ceil((len(self.bdot10k_cats_dict)+1)/5))
+            fig = plt.figure(figsize=(n*5, m*5))
+            plt.subplot(m,n,1)
+            plt.imshow(img[:,:,::-1])
+            
+            for i,vmax in enumerate([len(_) for _ in self.bdot10k_cats_dict.values()]):
+                plt.subplot(m,n,2+i)
+                plt.imshow(mask[i], cmap='gray')
+                plt.axis('off')
+                plt.title(cat_names[i]+'\n'+ self.bdot10k_df[self.bdot10k_df[self.level["code"]]==cat_names[i]].iloc[0][self.level["name"]])
+            plt.tight_layout()
+            if show:
+                plt.show()
+            else:
+                with io.BytesIO() as buff:
+                    fig.savefig(buff, format='raw')
+                    buff.seek(0)
+                    data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+                w, h = fig.canvas.get_width_height()
+                img = data.reshape((int(h), int(w), -1))
+                return img
+
+
+    def plot_sample_binary(self, img, mask, show=False):
         
-        n, m = 5, int(math.ceil((len(self.bdot10k_cats_dict)+1)/5))
+        n, m = 2,1
         fig = plt.figure(figsize=(n*5, m*5))
         plt.subplot(m,n,1)
         plt.imshow(img[:,:,::-1])
-        
-        for i,vmax in enumerate([len(_) for _ in self.bdot10k_cats_dict.values()]):
-            plt.subplot(m,n,2+i)
-            # print(np.unique(mask[i]))
-            plt.imshow(mask[i], cmap='gray')#,cmap='gist_ncar',vmin=0, vmax=vmax)
-            plt.axis('off')
-            plt.title(cat_names[i]+'\n'+ self.bdot10k_df[self.bdot10k_df[self.level["code"]]==cat_names[i]].iloc[0][self.level["name"]])
+
+        plt.subplot(m,n,2)
+        plt.imshow(mask[0], cmap='gray', vmin=0, vmax=1)
+        plt.axis('off')
+        plt.title(" ".join(self.bdot10k_cats_dict.keys()))
         plt.tight_layout()
         if show:
             plt.show()
